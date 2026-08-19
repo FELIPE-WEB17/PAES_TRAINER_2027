64d07db834793b2fc02bd397b4b4ece14af71d99
FELIPE-WEB17 <hector.pezoa.a33@gmail.com>
2026-08-18 21:19:49 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='paes-trainer-commercial-gamified-v2';
-const ASSETS=['./','./index.html','./styles.css','./config.js','./data.js','./generator.js','./app.js','./manifest.webmanifest'];
+const CACHE='paes-trainer-personal-classes-v1';
+const ASSETS=['./','./index.html','./styles.css','./config.js','./data.js','./generator.js','./app.js','./learning.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
