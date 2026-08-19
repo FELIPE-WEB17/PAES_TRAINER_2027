2d5ac968aef0c8d7aa00f9c041614dd627a3e9b9
FELIPE-WEB17 <hector.pezoa.a33@gmail.com>
2026-08-18 23:15:39 -0400

    PAES Trainer - pistas personalizadas por pregunta

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='paes-trainer-pedv2-grade-preu-v3';
-const ASSETS=['./','./index.html','./styles.css','./config.js','./data.js','./generator.js','./app.js','./learning.js','./classes.js','./manifest.webmanifest'];
+const CACHE='paes-trainer-pedv2-specific-hints-v4';
+const ASSETS=['./','./index.html','./styles.css','./config.js','./data.js','./generator.js','./app.js','./learning.js','./classes.js','./pedagogy.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
